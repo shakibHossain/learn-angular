b962f9c88b063e57cecf0c828dc0dfe80bb33237
shakibHossain <shossain@opusonesolutions.com>
2023-05-22 14:52:15 -0400

    Event binding.

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-products',
@@ -6,11 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent {
+  @ViewChild('myButton')
+  myButton!: ElementRef;
+
   allowNewProduct = false;
+  productCreationStatus = 'No product was created';
+  productName = '';
 
   constructor() {
     setTimeout(() => {
       this.allowNewProduct = true;
     }, 2000);
+  }
+
+  onCreateProduct() {
+    console.log('myButton', this.myButton.nativeElement);
+    this.productCreationStatus = 'Product was created';
+  }
+
+  onUpdateProductName(event: any) {
+    this.productName = event.target.value;
   }
 }
